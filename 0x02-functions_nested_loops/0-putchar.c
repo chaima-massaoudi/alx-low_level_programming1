65629eca67cdbae2dc7758dee4b21520e15ef7ad
chaima_massaoudi <chaimamassaoudi78@gmail.com>
2023-09-12 14:14:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
-	/**
+/**
 	 * main - print _putchar
 	 * is written by mbah
-	 * Return: 0
-	 */
+	 * Return: 
+*/
 
 
 	int main(void)
-	{
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +18,5 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
 
